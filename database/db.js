12c98cab2af27b6a0d69f8175d7ff8a36d46d901
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2024-06-13 12:47:18 +0500

    added console in db connection

--- database/db.js
+++ database/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDatabase = async () => {
     try {
+        console.log('Database trying to connect')
         const conn = await mongoose.connect(process.env.MONGO_URL)
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
